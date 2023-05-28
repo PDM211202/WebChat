e9c88c73e52c266703da7f4c2659361304965d0b
PDM2112 <phodinhmanh2002@gmail.com>
2023-05-28 11:28:54 +0700

    create function join room

--- app/public/js/client-chat.js
+++ app/public/js/client-chat.js
@@ -1,2 +1,14 @@
 // yêu cầu server kết nối với client
 const socket = io();
+
+// chuc nang lien quan den join room
+const queryString = location.search;
+
+const params = Qs.parse(queryString, {
+    ignoreQueryPrefix: true,
+});
+
+const { room, username } = params;
+
+// gui yeu cau ket noi den room ben server
+socket.emit("join room from client to server", { room, username });
--- app/server.js
+++ app/server.js
@@ -12,8 +12,34 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
+    socket.on("join room from client to server", ({ room, username }) => {
+        socket.join(room);
 
+        // gui cho client connection
+        socket.emit(
+            "send message from server to client",
+            createMessage(`Welcome to room ${room}`, "admin")
+        );
+
+        // gui cho cac client con lai
+        socket.broadcast
+            .to(room)
+            .emit(
+                "send message from server to client",
+                createMessage(`${username} vua tham gia vao phong`, "admin")
+            );
+
+        // ngat ket noi
+        socket.on("disconnect", () => {
+            removeUser(socket.id);
+            io.to(room).emit(
+                "send user list from server to client",
+                getUserList(room)
+            );
+        });
+    });
 });
+
 
 const port = 4000;
 server.listen(port, () => {
